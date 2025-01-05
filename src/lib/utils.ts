559dbc436c57bea1508085d7e0da17c0e99b71f9
anfezuar <anfezuar27_08@hotmail.com>
2025-01-05 14:41:14 -0500

    `feat: Update dependencies and add WhatsApp links to buttons`

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,8 @@
 export function cn(...classes: (string | undefined)[]) {
   return classes.filter(Boolean).join(" ");
 }
+
+export function generateWhatsAppLink(message: string) {
+  const encodedMessage = encodeURIComponent(message);
+  return `https://wa.me/573148464986?text=${encodedMessage}`;
+}
